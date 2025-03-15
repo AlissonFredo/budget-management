9323a83c16dd4c09afd5e7bbd421d53e482ae33d
AlissonFredo <douglasalissonsf@gmail.com>
2025-03-15 00:05:04 -0300

    feat: add request to fetch row from Google Sheet

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,25 @@
 function App() {
+  var query_params = new URLSearchParams({
+    limit: 10,
+    // query_type: "and",
+    // nome: "example value",
+    // categoria: "example value",
+    // data: "example value",
+    // valor: "example value",
+  });
+  var url =
+    "https://sheet2api.com/v1/rtjzbZKQ2CY1/budget-management/P%C3%A1gina1?" +
+    query_params;
+
+  fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log("Success:", data);
+    })
+    .catch((error) => {
+      console.error("Error:", error);
+    });
+
   return (
     <>
       <h1>Budget Management</h1>
