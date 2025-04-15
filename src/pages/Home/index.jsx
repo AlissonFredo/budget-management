1447aa55370a12f87ba7c6c247b32ab9f3e74b7b
AlissonFredo <douglasalissonsf@gmail.com>
2025-04-14 23:30:32 -0300

    feat: add multiple bar chart to compare incomes and outgoings
    - Adds a multiple bar chart to visually compare income and outgoing transactions.

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -14,6 +14,16 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 
+import { Bar, BarChart, CartesianGrid, XAxis } from "recharts";
+
+import {
+  ChartContainer,
+  ChartLegend,
+  ChartLegendContent,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+
 function Home() {
   const [transactions, setTransactions] = useState([]);
 
@@ -77,12 +87,38 @@ function Home() {
     .filter((transaction) => transaction?.type == "incoming")
     .reduce((sum, transaction) => sum + transaction.amount, 0);
 
+  const chartData = [
+    { month: "January", income: 186, outgoing: 80 },
+    { month: "February", income: 305, outgoing: 200 },
+    { month: "March", income: 237, outgoing: 120 },
+    { month: "April", income: 73, outgoing: 190 },
+    { month: "May", income: 209, outgoing: 130 },
+    { month: "June", income: 214, outgoing: 140 },
+    { month: "July", income: 214, outgoing: 140 },
+    { month: "August", income: 214, outgoing: 140 },
+    { month: "September", income: 214, outgoing: 140 },
+    { month: "October", income: 214, outgoing: 140 },
+    { month: "November", income: 214, outgoing: 140 },
+    { month: "December", income: 214, outgoing: 140 },
+  ];
+
+  const chartConfig = {
+    income: {
+      label: "Income",
+      color: "#7dcf9c",
+    },
+    outgoing: {
+      label: "Expenses",
+      color: "#f87171",
+    },
+  };
+
   return (
     <Container>
       <Header />
       <SummaryCards incoming={incoming} outgoing={outgoing} />
 
-      <section className="grid grid-cols-1 md:grid-cols-4 gap-8">
+      <section className="grid grid-cols-1 md:grid-cols-4 gap-8 mb-8">
         <Filter
           totalTransactions={transactions.length}
           selectedMonth={selectedMonth}
@@ -117,6 +153,48 @@ function Home() {
                 setTransactions(filteredTransactions)
               }
             />
+          </CardContent>
+        </Card>
+      </section>
+
+      <section className="grid grid-cols-1 md:grid-cols-4 gap-8">
+        <Card className="md:col-span-2">
+          <CardHeader className="border-b">
+            <div className="flex flex-col sm:flex-row justify-between items-start sm:items-center">
+              <div>
+                <CardTitle className="text-2xl">
+                  Incomes & Expenses (Yearly)
+                </CardTitle>
+                <CardDescription>
+                  Annual comparison between income and expenses
+                </CardDescription>
+              </div>
+            </div>
+          </CardHeader>
+          <CardContent>
+            <ChartContainer
+              config={chartConfig}
+              className="min-h-[200px] w-full"
+            >
+              <BarChart accessibilityLayer data={chartData}>
+                <CartesianGrid vertical={false} />
+                <XAxis
+                  dataKey="month"
+                  tickLine={false}
+                  tickMargin={10}
+                  axisLine={false}
+                  tickFormatter={(value) => value.slice(0, 3)}
+                />
+                <ChartTooltip content={<ChartTooltipContent />} />
+                <ChartLegend content={<ChartLegendContent />} />
+                <Bar dataKey="income" fill="var(--color-income)" radius={4} />
+                <Bar
+                  dataKey="outgoing"
+                  fill="var(--color-outgoing)"
+                  radius={4}
+                />
+              </BarChart>
+            </ChartContainer>
           </CardContent>
         </Card>
       </section>
